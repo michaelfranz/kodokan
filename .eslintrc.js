f6f84c9281b9aa009888d1a907519906e19c5723
John Carpio <john.c.carpio@gmail.com>
2019-11-30 21:50:11 +0800

    setup eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,68 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'fbjs',
+    'prettier',
+  ],
+  parser: 'babel-eslint',
+  plugins: ['react', 'sort-imports-es6-autofix', 'prettier'],
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+      classes: true,
+      objectLiteralShorthandMethods: true,
+      experimentalObjectRestSpread: true,
+      arrowFunctions: true,
+      modules: true,
+    },
+    sourceType: 'module',
+  },
+  rules: {
+    semi: 0,
+    'max-len': [
+      'error',
+      {
+        code: 80,
+      },
+    ],
+    indent: 0,
+    'no-constant-condition': 0,
+    'no-extra-semi': 0,
+    'no-unused-vars': 2,
+    'no-undef': 2,
+    'react/jsx-uses-vars': 2,
+    'react/jsx-key': 2,
+    'react/jsx-no-duplicate-props': 2,
+    'react/jsx-no-undef': 2,
+    'react/jsx-uses-react': 2,
+    'no-irregular-whitespace': 2,
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      'error',
+      {
+        ignoreCase: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        parser: 'babylon',
+        semi: false,
+        requirePragma: false,
+        tabWidth: 2,
+        useTabs: false,
+      },
+    ],
+  },
+}
