0f19eaedcc44e000de35d4193ef499689d32fc14
John Carpio <john.c.carpio@gmail.com>
2019-11-30 23:29:25 +0800

    Revert "WIP - android splash screen"
    This reverts commit 256b2b5677f4649b593bff0e36568b6f44ed8586.

--- android/app/src/main/java/com/kodokanpro/MainApplication.java
+++ android/app/src/main/java/com/kodokanpro/MainApplication.java
@@ -1,10 +1,10 @@
-package com.kodokanpro;
+package com.kodokanpropro;
 
 import android.app.Application;
 import android.content.Context;
-import com.facebook.react.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
