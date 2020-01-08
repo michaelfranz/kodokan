c4986887bf973c478245a690081b0e8bb64adc32
John Carpio <john.c.carpio@gmail.com>
2020-01-09 04:07:47 +0800

    use new version of RN-elements and vector-icons in rendering searchbar

--- src/dictionary/index.tsx
+++ src/dictionary/index.tsx
@@ -1,16 +1,24 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, ImageBackground } from 'react-native'
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native'
 import withScreenLayout, { Props } from '../common/withScreenLayout'
 import {
   FOREGROUND_COLOUR_ALT,
   BACKGROUND_COLOUR,
   FOREGROUND_COLOUR,
 } from '../theme/colours'
-// import { SearchBar } from 'react-native-elements'
+import { SearchBar } from 'react-native-elements'
 import { strings } from '../locales/i18n'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { styles as fontStyles, Text } from '../common/text'
 
 const BackgroundPortrait = require('../images/background1P.png')
 const BackgroundLandscape = require('../images/background1L.png')
+const dismissKeyboard = require('react-native-dismiss-keyboard')
 
 const styles = StyleSheet.create({
   container: {
@@ -25,6 +33,11 @@ const styles = StyleSheet.create({
     opacity: 0.5,
     resizeMode: 'cover',
   },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 5,
+  },
   searchBar: {
     backgroundColor: 'transparent',
     borderBottomWidth: 0,
@@ -37,11 +50,75 @@ const styles = StyleSheet.create({
     borderColor: 'rgb(199,200,204)',
     color: FOREGROUND_COLOUR,
   },
+  searchIconContainer: {
+    padding: 0,
+  },
 })
 
 const DictionaryScreen = ({ orientation }): React.ReactElement<Props> => {
   const isLandscape = orientation === 'landscape'
   const [searchText, setSearchText] = useState('')
+  const hasSearchText = !!searchText.trim().length
+  const [bookmarkDisplayMode, setBookmarkDisplayMode] = useState(false)
+
+  const onChangeText = (value: string) => {
+    setSearchText(value)
+  }
+
+  const toggleBookmarkDisplay = () => {
+    setBookmarkDisplayMode(!bookmarkDisplayMode)
+  }
+
+  const renderHeader = (): JSX.Element => {
+    return (
+      <View style={styles.headerContainer}>
+        <SearchBar
+          autoCapitalize={'none'}
+          platform="ios"
+          autoCorrect={false}
+          clearIcon={{
+            color: FOREGROUND_COLOUR_ALT,
+            name: 'md-close',
+          }}
+          containerStyle={styles.searchBar}
+          inputStyle={[styles.inputStyle, fontStyles.baseText]}
+          placeholder={strings('Search')}
+          style={styles.searchBar}
+          cancelButtonProps={{
+            color: FOREGROUND_COLOUR,
+            buttonTextStyle: fontStyles.baseText,
+          }}
+          onChangeText={onChangeText}
+          onClear={() => {
+            onChangeText('')
+            dismissKeyboard()
+          }}
+          value={searchText}
+          showCancel={false}
+        />
+
+        <TouchableOpacity onPress={toggleBookmarkDisplay}>
+          <Icon
+            name={bookmarkDisplayMode ? 'bookmark' : 'bookmark-o'}
+            size={28}
+            color={FOREGROUND_COLOUR}
+          />
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  const renderBody = (): JSX.Element => {
+    return (
+      <View style={{ flex: 1 }}>
+        {bookmarkDisplayMode && !hasSearchText && <Text>Bookmarks</Text>}
+        {!bookmarkDisplayMode && !hasSearchText && (
+          <Text>Recents and Term of the Day</Text>
+        )}
+        {hasSearchText && <Text>Search results list</Text>}
+      </View>
+    )
+  }
 
   return (
     <View style={styles.container}>
@@ -50,17 +127,8 @@ const DictionaryScreen = ({ orientation }): React.ReactElement<Props> => {
         style={styles.backgroundImageContainer}
         imageStyle={styles.backgroundImage}
       >
-        <View style={{ flexDirection: 'row' }}>
-          {/* <SearchBar
-            autoCapitalize={'none'}
-            autoCorrect={false}
-            clearIcon={{ color: FOREGROUND_COLOUR_ALT, name: 'clear' }}
-            containerStyle={styles.searchBar}
-            inputStyle={styles.inputStyle}
-            placeholder={strings('Search')}
-            style={styles.searchBar}
-          /> */}
-        </View>
+        {renderHeader()}
+        {renderBody()}
       </ImageBackground>
     </View>
   )
