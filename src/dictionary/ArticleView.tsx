702865ffd04812a646447b9923e092be7ce2f0b5
John Carpio <john.c.carpio@gmail.com>
2020-06-29 20:25:54 +0800

    fix ellipsis for long words in homescreen

--- src/dictionary/ArticleView.tsx
+++ src/dictionary/ArticleView.tsx
@@ -59,6 +59,8 @@ const ArticleView = ({
     navigate(screen, { term })
   }
 
+  const ICON_WIDTH = 32
+
   useEffect(() => {
     const term = article.name
     BookmarkInfo.isBookmarked(term).then(value => {
@@ -76,14 +78,13 @@ const ArticleView = ({
   ) => {
     return show ? (
       <TouchableOpacity
-        style={{ paddingRight: 8 }}
+        key={screen}
+        style={{ width: ICON_WIDTH }}
         onPress={() => showScreen(screen, term)}
       >
         <Image source={image} />
       </TouchableOpacity>
-    ) : (
-      <Text />
-    )
+    ) : null
   }
 
   const toggleBookmark = async () => {
@@ -101,7 +102,7 @@ const ArticleView = ({
 
   const renderBookmarkButton = () => {
     return (
-      <TouchableOpacity onPress={toggleBookmark}>
+      <TouchableOpacity key="bookmark" onPress={toggleBookmark}>
         <Text style={{ margin: 8, fontSize: 15, textAlign: 'left' }}>
           <Icon
             name={isBookmarked ? 'bookmark' : 'bookmark-o'}
@@ -125,6 +126,13 @@ const ArticleView = ({
   const giButton = renderNaviButton(isGiTerm, 'Gi', name, ImageDictGi)
   const bookmarkButton = renderBookmarkButton()
 
+  const actionIcons = [
+    gokyoButton,
+    wazaButton,
+    giButton,
+    bookmarkButton,
+  ].filter(actionIcon => !!actionIcon)
+
   return (
     <View
       style={[
@@ -139,27 +147,33 @@ const ArticleView = ({
       ]}
     >
       <TouchableOpacity
-        style={{ flexDirection: 'row', alignItems: 'center' }}
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          flex: 1,
+          paddingRight: 5,
+        }}
         onPress={onPress}
       >
         <Icon name="volume-up" size={28} color={FOREGROUND_COLOUR} />
-        <View style={{ paddingLeft: 4 }}>
-          <H2 style={styles.techniqueNameText}>{article.displayName}</H2>
-          <Text style={styles.translationText}>{article.translation}</Text>
+        <View style={{ paddingLeft: 4, flex: 1 }}>
+          <H2 numberOfLines={1} style={styles.techniqueNameText}>
+            {article.displayName}
+          </H2>
+          <Text numberOfLines={1} style={styles.translationText}>
+            {article.translation}
+          </Text>
         </View>
       </TouchableOpacity>
       <View
         style={{
           alignItems: 'center',
-          flex: 1,
+          width: actionIcons.length * ICON_WIDTH,
           flexDirection: 'row',
           justifyContent: 'flex-end',
         }}
       >
-        {gokyoButton}
-        {wazaButton}
-        {giButton}
-        {bookmarkButton}
+        {actionIcons.map(actionIcon => actionIcon)}
       </View>
     </View>
   )
--- src/dictionary/index.tsx
+++ src/dictionary/index.tsx
@@ -1,4 +1,4 @@
-import Amplify, { Analytics } from 'aws-amplify' 
+import Amplify, { Analytics } from 'aws-amplify'
 import React, { useState, useEffect } from 'react'
 import {
   View,
@@ -196,8 +196,7 @@ const DictionaryScreen = ({
   }
 
   const renderHeader = (): JSX.Element => {
-
-    Analytics.record({name: 'dictionaryVisit'})
+    Analytics.record({ name: 'dictionaryVisit' })
 
     return (
       <View style={styles.headerContainer}>
